5e63c56e98f912095e1336c57f9a709c76363012
Tusry Uong <uongthanhtu5@gmail.com>
2025-08-12 22:51:08 +0700

    feat: add and define student and course

--- university-registration/src/main/java/com/tusry/universityregistration/course/domain/Course.java
+++ university-registration/src/main/java/com/tusry/universityregistration/course/domain/Course.java
@@ -1,4 +1,16 @@
 package com.tusry.universityregistration.course.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Course {
+    @Id
+    String code;
+    String name;
 }
--- university-registration/src/main/java/com/tusry/universityregistration/student/domain/Student.java
+++ university-registration/src/main/java/com/tusry/universityregistration/student/domain/Student.java
@@ -1,4 +1,19 @@
 package com.tusry.universityregistration.student.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
 }
